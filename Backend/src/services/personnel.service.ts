b90a4533eb39f79c1c9ee98e8dd6e61a9c9c349b
JeffriUI <113225717+reg-strtgy-enjyr@users.noreply.github.com>
2024-09-08 21:03:39 +0700

    updated personnel services on backend
    updated personnel services to include status number and message

--- Backend/src/services/personnel.service.ts
+++ Backend/src/services/personnel.service.ts
@@ -33,11 +33,16 @@ export class PersonnelService {
     Personnel
   */
   async addPersonnel(createPersonnelDto: CreatePersonnelDto) {
-    return this.prisma.tbl_personnel.create({
+    const personnel = await this.prisma.tbl_personnel.create({
       data: {
         ...createPersonnelDto,
       },
     });
+    return {
+      status: 200,
+      message: 'Personnel Created',
+      personnel
+    }
   }
 
   async updatePersonnel(updatePersonnelDto: UpdatePersonnelDto) {
@@ -51,10 +56,16 @@ export class PersonnelService {
       throw new NotFoundException(`Personnel with ID ${person_id} not found`);
     }
 
-    return this.prisma.tbl_personnel.update({
+    const newPersonnel = await this.prisma.tbl_personnel.update({
       where: { person_id },
       data,
     });
+
+    return {
+      status: 200,
+      message: 'Personnel Updated',
+      newPersonnel
+    }
   }
 
   async deletePersonnel(deletePersonnelDto: DeletePersonnelDto) {
@@ -68,9 +79,14 @@ export class PersonnelService {
       throw new NotFoundException(`Personnel with ID ${person_id} not found`);
     }
 
-    return this.prisma.tbl_personnel.delete({
+    await this.prisma.tbl_personnel.delete({
       where: { person_id },
     });
+
+    return {
+      status: 200,
+      message: 'Personnel deleted'
+    }
   }
 
   async showPersonnelById(showPersonnelByIdDto: ShowPersonnelByIdDto) {
@@ -90,11 +106,15 @@ export class PersonnelService {
       throw new NotFoundException(`Personnel with ID ${person_id} not found`);
     }
 
-    return personnel;
+    return {
+      status: 200,
+      message: 'Personnel found',
+      showProduct: personnel
+    };
   }
 
   async showAllPersonnel() {
-    return this.prisma.tbl_personnel.findMany({
+    const personnels = await this.prisma.tbl_personnel.findMany({
       include: {
         certification: true,
         education: true,
@@ -102,11 +122,17 @@ export class PersonnelService {
         training_record: true,
       },
     });
+    return {
+      status: 200,
+      message: 'Showing Personnels',
+      showProduct: personnels
+    }
   }
 
   async searchPersonnel(searchPersonnelDto: SearchPersonnelDto) {
     const input = searchPersonnelDto.input;
-    return this.prisma.tbl_personnel.findMany({
+
+    const personnels = await this.prisma.tbl_personnel.findMany({
       where: {
         OR: [
           { person_name: { contains: input, mode: 'insensitive' } },
@@ -114,118 +140,203 @@ export class PersonnelService {
         ],
       },
     });
+
+    return {
+      status: 200,
+      message:
+        personnels.length > 0
+          ? 'Showing result of Personnel'
+          : 'No Data Personnel',
+      showProduct: personnels,
+    }; 
   }
 
   /* 
     Education
   */
   async addEducation(addEducationDto: AddEducationDto) {
-    return this.prisma.education.create({
+    const education = await this.prisma.education.create({
       data: addEducationDto,
     });
+    return {
+      status: 200,
+      message: 'Education Created',
+      education
+    }
   }
 
   async updateEducation(updateEducationDto: UpdateEducationDto) {
     const { edu_id, ...data } = updateEducationDto;
-    return this.prisma.education.update({
+    const education = await this.prisma.education.update({
       where: { edu_id },
       data,
     });
+    return {
+      status: 200,
+      message: 'Education updated',
+      education
+    }
   }
 
   async deleteEducation(deleteEducationDto: DeleteEducationDto) {
-    return this.prisma.education.delete({
+    await this.prisma.education.delete({
       where: { edu_id: deleteEducationDto.edu_id },
     });
+    return {
+      status: 200,
+      mesage: 'Education deleted'
+    }
   }
 
   async showEducationById(showEducationById: ShowEducationByIdDto) {
     const { person_id } = showEducationById;
-    return this.prisma.education.findMany({
+    const educations = await this.prisma.education.findMany({
       where: { person_id },
     });
+    return {
+      status: 200,
+      message: 'Showing Education',
+      showProduct: educations
+    }
   }
 
   /* Experience Management */
   async addExperience(addExperienceDto: AddExperienceDto) {
-    return this.prisma.experience_record.create({
+    const experience = await this.prisma.experience_record.create({
       data: addExperienceDto,
     });
+    return {
+      status: 200,
+      message: 'Experience Created',
+      experience
+    }
   }
 
   async updateExperience(updateExperienceDto: UpdateExperienceDto) {
     const { experience_id, ...data } = updateExperienceDto;
-    return this.prisma.experience_record.update({
+    const experience = await this.prisma.experience_record.update({
       where: { experience_id },
       data,
     });
+    return {
+      status: 200,
+      message: 'Experience updated',
+      experience
+    }
   }
 
   async deleteExperience(deleteExperienceDto: DeleteExperienceDto) {
-    return this.prisma.experience_record.delete({
+    await this.prisma.experience_record.delete({
       where: { experience_id: deleteExperienceDto.experience_id },
     });
+    return {
+      status: 200,
+      message: 'Experience deleted'
+    }
   }
 
   async showExperienceByPersonId(showExperienceById: ShowExperienceByIdDto) {
     const { person_id } = showExperienceById;
-    return this.prisma.experience_record.findMany({
+    const experiences = await this.prisma.experience_record.findMany({
       where: { person_id },
     });
+    return {
+      status: 200,
+      message: 'Showing experience',
+      showProduct: experiences
+    }
   }
 
   /* Training Management */
   async addTraining(addTrainingDto: AddTrainingDto) {
-    return this.prisma.training_record.create({
+    const training = await this.prisma.training_record.create({
       data: addTrainingDto,
     });
+    return {
+      status: 200,
+      message: 'Training Created',
+      training
+    }
   }
 
   async updateTraining(updateTrainingDto: UpdateTrainingDto) {
     const { training_id, ...data } = updateTrainingDto;
-    return this.prisma.training_record.update({
+    const training = await this.prisma.training_record.update({
       where: { training_id },
       data,
     });
+    return {
+      status: 200,
+      message: 'Training updated',
+      training
+    }
   }
 
   async deleteTraining(deleteTrainingDto: DeleteTrainingDto) {
-    return this.prisma.training_record.delete({
+    await this.prisma.training_record.delete({
       where: { training_id: deleteTrainingDto.training_id },
     });
+    return {
+      status: 200,
+      message: 'Training updated'
+    }
   }
 
   async showTrainingByPersonId(showTrainingById: ShowTrainingByIdDto) {
     const { person_id } = showTrainingById;
-    return this.prisma.training_record.findMany({
+    const trainings = await this.prisma.training_record.findMany({
       where: { person_id },
     });
+    return {
+      status: 200,
+      message: 'Showing Training',
+      showProduct: trainings
+    }
   }
 
   async addCert(addCertDto: AddCertDto) {
-    return this.prisma.certification.create({
+    const cert = await this.prisma.certification.create({
       data: addCertDto,
     });
+    return {
+      status: 200,
+      message: 'Certification Created',
+      cert
+    }
   }
 
   async updateCert(updateCertDto: UpdateCertDto) {
     const { cert_id, ...data } = updateCertDto;
-    return this.prisma.certification.update({
+    const cert = await this.prisma.certification.update({
       where: { cert_id },
       data,
     });
+    return {
+      status: 200,
+      message: 'Certification updated',
+      cert
+    }
   }
 
   async deleteCert(deleteCertDto: DeleteCertDto) {
-    return this.prisma.certification.delete({
+    await this.prisma.certification.delete({
       where: { cert_id: deleteCertDto.cert_id },
     });
+    return {
+      status: 200,
+      message: 'Certification deleted'
+    }
   }
 
   async showCertByPersonId(showCertById: ShowCertByIdDto) {
     const { person_id } = showCertById;
-    return this.prisma.certification.findMany({
+    const certs = await this.prisma.certification.findMany({
       where: { person_id },
     });
+    return {
+      status: 200,
+      message: 'Showing Certifications',
+      showProduct: certs
+    }
   }
 }
